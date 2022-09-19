f3a74e4da1533fba168a0d928d080a19379881f9
Kavita Pathak <110056559+Kunalpathak2004@users.noreply.github.com>
2022-09-19 22:20:02 +0530

    feat-translate

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-var uswername=prompt("Give me your username")
-alert("this website works" + username);
+var btnTranslate=document.querySelector("#btn-translate");
+// The Document interface represents any web page loaded in the browser and serves as an entry point into the web page's content, which is the DOM tree.
+// The Document querySelector() returns the first element within in the document that matches the specified selectors. For eg: here it returned the selector with id btn-translate which was the id given for button
+
+btnTranslate.addEventListener("click" , function clickEventHandler(){
+    console.log("clicked")
+})
