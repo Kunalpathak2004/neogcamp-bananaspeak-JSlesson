b5da25a8194832395ccd461749068eae0b9cc72c
Kavita Pathak <110056559+Kunalpathak2004@users.noreply.github.com>
2022-09-20 10:49:10 +0530

    feat-textarea updates

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-var btnTranslate=document.querySelector("#btn-translate");
+var btnTranslate = document.querySelector("#btn-translate");
 // The Document interface represents any web page loaded in the browser and serves as an entry point into the web page's content, which is the DOM tree.
 // The Document querySelector() returns the first element within in the document that matches the specified selectors. For eg: here it returned the selector with id btn-translate which was the id given for button
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput);
 
-btnTranslate.addEventListener("click" , function clickEventHandler(){
-    console.log("clicked")
-})
+
+function clickHandler(){
+    console.log("clicked");
+    console.log("input" , txtInput.value);
+}
+
+
+
+btnTranslate.addEventListener("click", clickHandler)
