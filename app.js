c088de5afa247e96a887a9e3613568852df646b2
Kavita Pathak <110056559+Kunalpathak2004@users.noreply.github.com>
2022-09-22 11:15:12 +0530

    feat-updates on translation and error handling

--- app.js
+++ app.js
@@ -3,15 +3,30 @@ var btnTranslate = document.querySelector("#btn-translate");
 // The Document querySelector() returns the first element within in the document that matches the specified selectors. For eg: here it returned the selector with id btn-translate which was the id given for button
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+
+function getTrabslateUrl(text) {
+    return serverUrl + "?" + "text= " + text
+}
+function errorHandler(error){
+    console.log("error occured" , error);
+    alert("server is down! try again after sometime");
+}
 
 
 
-function clickHandler(){
-    outputDiv.innerText="aggjhdamhnsafhg " + txtInput.value;
+function clickHandler() {
+    outputDiv.innerText = "aggjhdamhnsafhg " + txtInput.value;
+
+    // when you have to check the value or assign it you use 
+    // .value but when we have to acces inner text we use     .innerText
+    var inputText = txtInput.value; //taking input
+    //   processing output
+    fetch(getTrabslateUrl(inputText))
+        .then(res => res.json())
+        .then(json => console.log(json.contents.translated))
+        .catch(errorHandler)
 };
-// when you have to check the value or assign it you use 
-// .value but when we have to acces inner text we use     .innerText
-
-
 
 btnTranslate.addEventListener("click", clickHandler)
